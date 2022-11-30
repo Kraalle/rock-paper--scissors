87b20321a5ccef3c56a63da5280c84de7007efff
Max McLaren <maxtova@yahoo.com>
2022-11-30 16:22:50 -0700

    final app.js and index.html and style.css

--- app.js
+++ app.js
@@ -1,3 +1,65 @@
+let playerPoints = 0;
+let computerPoints = 0;
+
+const showPlayerPoints = document.querySelector('#player-points');
+const showComputerPoints = document.querySelector('#computer-points');
+const showGameWinner = document.querySelector('#game-winner');
+const winner = document.querySelector('#winner');
+const btnRock = document.querySelector('#btn-rock');
+const btnRestart = document.querySelector('#restart-button');
+
+btnRestart.addEventListener('click', () => {
+    restart();
+    showPlayerPoints.textContent = playerPoints;
+    showComputerPoints.textContent = computerPoints
+});
+
+btnRock.addEventListener('click', () => {
+    if (playerPoints >= 5 || computerPoints >= 5) {
+        alert("Game is over. Refresh page to play again.")
+    }
+    let computerChoice = getComputerChoice();
+    let playerSelection = "Rock";
+    let roundWinner = playRound(playerSelection, computerChoice);
+    winner.textContent = roundWinner;
+    showPlayerPoints.textContent = playerPoints;
+    showComputerPoints.textContent = computerPoints
+    let finalWinner = gameWinner(playerPoints, computerPoints);
+    showGameWinner.textContent = finalWinner;
+})
+
+const btnPaper = document.querySelector('#btn-paper');
+btnPaper.addEventListener('click', () => {
+    if (playerPoints >= 5 || computerPoints >= 5) {
+        alert("Game is over. Refresh page to play again.")
+    } else {
+        let computerChoice = getComputerChoice();
+        let playerSelection = "Paper";
+        let roundWinner = playRound(playerSelection, computerChoice);
+        winner.textContent = roundWinner;
+        showPlayerPoints.textContent = playerPoints;
+        showComputerPoints.textContent = computerPoints
+        let finalWinner = gameWinner(playerPoints, computerPoints);
+        showGameWinner.textContent = finalWinner;
+    }
+})
+
+const btnScissors = document.querySelector('#btn-scissors');
+btnScissors.addEventListener('click', () => {
+    if (playerPoints >= 5 || computerPoints >= 5) {
+        alert("Game is over. Refresh page to play again.")
+    } else {
+        let computerChoice = getComputerChoice();
+        let playerSelection = "Scissors";
+        let roundWinner = playRound(playerSelection, computerChoice);
+        winner.textContent = roundWinner;
+        showPlayerPoints.textContent = playerPoints;
+        showComputerPoints.textContent = computerPoints
+        let finalWinner = gameWinner(playerPoints, computerPoints);
+        showGameWinner.textContent = finalWinner;
+    }
+})
+
 function getComputerChoice() {
     let choice;
     randomNumber = Math.floor(Math.random() * 3);
@@ -12,33 +74,55 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    let tie = `Tie! ${playerSelection} and ${computerSelection} are the same`;
-    let playerWin = `You win! ${playerSelection} beats ${computerSelection}`;
-    let computerWin = `You win! ${playerSelection} beats ${computerSelection}`
+    const tie = `Tie! ${playerSelection} and ${computerSelection} are the same`;
+    const playerWin = `You win! ${playerSelection} beats ${computerSelection}`;
+    const computerWin = `You lose! ${playerSelection} loses to ${computerSelection}`;
+    let winner = false;
+
     if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
         return tie;
     } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "SCISSORS") {
-        return playerWin;
+        console.log("hello");
+        playerPoints += 1;
+        winner = true;
     } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "PAPER") {
+        console.log("hello");
+        playerPoints += 1;
         return playerWin;
     } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK") {
-        return playerWin;
+        console.log("hello");
+        playerPoints += 1;
+        winner = true;
     } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "SCISSORS") {
-        return computerWin;
+        computerPoints += 1;
+        winner = false;
     } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "PAPER") {
-        return computerWin; 
+        computerPoints += 1;
+        winner = false; 
     } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "ROCK") {
-        return computerWin;
+        computerPoints += 1;
+        winner = false;
+    }
+    if (winner === true) {
+        return playerWin;
+    } else {
+        return computerWin
     }
 }
 
-function game() {
-    for(let i = 0; i < 5; i++) {
-        let computerChoice = getComputerChoice();
-        let userInput = prompt("Rock, paper, or scissors?: ")
-        result = playRound(userInput, computerChoice);
-        console.log(result);
+const gameWinner = (playerPoints, computerPoints) => {
+    const playerWonRound = `Congratulations! You won this game of Rock Paper Scissors!`;
+    const computerWonRound = 'Tough luck... You lost to a computer!';
+    if (playerPoints === 5) {
+        return playerWonRound;
+    } else if (computerPoints === 5){
+        return computerWonRound;
     }
 }
 
-game();
+const restart = () => {
+    playerPoints = 0;
+    computerPoints = 0;
+
+};
+
